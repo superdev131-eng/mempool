superdev131-eng/mempool

commit ecbd18087b267ba61b74477c973281b740edaf30

Effective fee could go below 0 due to rounding error. (#623)
fixes #606

backend/src/api/common.ts
@@ -105,7 +105,7 @@ export class Common {
       totalFees += tx.bestDescendant.fee;
     }
 
-    tx.effectiveFeePerVsize = totalFees / (totalWeight / 4);
+    tx.effectiveFeePerVsize = Math.max(1, totalFees / (totalWeight / 4));
     tx.cpfpChecked = true;
 
     return {

commit 3808b26a6409c73530b107f9fdeae063a1fdbc02

Fixes the minium tx fee bug introduced in ecbd18087b267ba61b74477c973281b740edaf30

backend/src/api/common.ts
@@ -114,7 +114,7 @@ export class Common {
       totalFees += tx.bestDescendant.fee;
     }
 
-    tx.effectiveFeePerVsize = Math.max(Common.isLiquid() ? 0.1 : 1, totalFees / (totalWeight / 4));
+    tx.effectiveFeePerVsize = Math.max(0, totalFees / (totalWeight / 4));
     tx.cpfpChecked = true;
 
     return {